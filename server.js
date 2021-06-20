818798cbc330c1d854169d004ae878d558d0f471
Jillian Amelung <jpamelung@gmail.com>
2021-06-20 00:33:21 -0400

    added findbyID function

--- server.js
+++ server.js
@@ -42,7 +42,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 app.get('/api/animals', (req, res) => {
@@ -51,6 +54,11 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
   });
 
   app.listen(PORT, () => {
